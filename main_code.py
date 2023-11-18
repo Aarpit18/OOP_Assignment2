8bb4bb48f9d6061552708652bd574d1ad4f9793d
Aarpit18 <manay087@mymail.unisa.edu.au>
2023-11-18 12:04:56 +1030

    Attributes of Alchemist class entered

--- main_code.py
+++ main_code.py
@@ -1,1 +1,10 @@
 class Alchemist:
+    def __init__(self):
+        self.__attack = 0
+        self.__strength = 0
+        self.__denfense = 0
+        self.__magic = 0
+        self.__range = 0
+        self.__necromancy = 0
+        self.__laboratory = Laboratory()
+        self.recipes: []
